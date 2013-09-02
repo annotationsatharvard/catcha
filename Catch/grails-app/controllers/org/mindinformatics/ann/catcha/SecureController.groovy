acc0e296a0d34ed09e99d22c93074b92037373ff
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2013-09-02 16:44:53 -0400

    Missing upload action in controller

--- Catch/grails-app/controllers/org/mindinformatics/ann/catcha/SecureController.groovy
+++ Catch/grails-app/controllers/org/mindinformatics/ann/catcha/SecureController.groovy
@@ -5,4 +5,18 @@ class SecureController {
 	def home = {
 		render (view: "home")
 	}
+	
+	def upload = {
+		render (view: "upload")
+	}
+	
+	def uploadAnnotationFile() {
+		def f = request.getFile('myFile')
+		if (f.empty) {
+			flash.message = 'file cannot be empty'
+			render(view: 'uploadForm')
+			return
+		}
+	
+	f.transferTo(new File('/some/local/dir/myfile.txt')) response.sendError(200, 'Done') }
 }

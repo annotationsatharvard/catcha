5844a0a65abce6f7c3390c0a47cc21acb78e58e0
jmiranda <jmiranda@pih.org>
2014-01-06 09:56:29 -0500

    fixed #30: configure mysql data source for development and production environment

--- Catch/grails-app/conf/DataSource.groovy
+++ Catch/grails-app/conf/DataSource.groovy
@@ -1,9 +1,3 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -13,30 +7,66 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+            pooled = true
+            //dbCreate = "create-drop"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            url = "jdbc:mysql://localhost:3306/catch?useUnicode=yes&characterEncoding=UTF-8&autoReconnect=true"
+            username = "catch"
+            password = "catch"
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 15
+                initialSize = 10
+                numTestsPerEvictionRun = 3
+                maxWait = 10000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "select now()"
+                minEvictableIdleTimeMillis = 1000 * 60 * 5
+                timeBetweenEvictionRunsMillis = 1000 * 60 * 5
+            }
+
+
+//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
         }
     }
     test {
         dataSource {
+            pooled = true
+            driverClassName = "org.h2.Driver"
+            username = "sa"
+            password = ""
             dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+
         }
     }
     production {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
+            //dbCreate = "create-drop"
             pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            url = "jdbc:mysql://localhost:3306/catch?useUnicode=yes&characterEncoding=UTF-8&autoReconnect=true"
+            username = "catch"
+            password = "catch"
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 15
+                initialSize = 10
+                numTestsPerEvictionRun = 3
+                maxWait = 10000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "select now()"
+                minEvictableIdleTimeMillis = 1000 * 60 * 5
+                timeBetweenEvictionRunsMillis = 1000 * 60 * 5
             }
         }
     }

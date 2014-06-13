dd1d99d143bbcb255110df040dd74f241394c770
jmiranda <jcm62@columbia.edu>
2014-06-13 08:22:51 -0400

    Added support for the firebase token format

--- Catch/grails-app/conf/AuthTokenFilters.groovy
+++ Catch/grails-app/conf/AuthTokenFilters.groovy
@@ -17,8 +17,12 @@ class AuthTokenFilters {
                 else {
                     try {
 
+                        // Annotator token format
                         //header: {"alg":"HS256","cty":"text\/plain","typ":"JWS"}
                         //payload: {"consumerKey":"openannotation","issuedAt":"2013-08-31T06:34:42-0400","userId":"jmiranda","jti":"0f4ce532-dbb3-4715-8601-d3fc34d9b460","ttl":86400,"iat":1377988482}
+
+                        //
+                        //payload: {"iat": 1396290995, "d": {"issuedAt": "2014-03-31T14:36:35.017866-4:00", "consumerKey": "cbdf435b-a609-4126-b58a-589c40075075", "userId": "", "ttl": 86400}, "v": 0}
 
                         JWSObject jwsObject = JWSObject.parse(token)
                         println "header: " + jwsObject.header
@@ -27,7 +31,8 @@ class AuthTokenFilters {
                         println "signature: " + jwsObject.signature
 
                         // 1. Make sure an API exists with this name
-                        def consumerKey = jwsObject.payload.toJSONObject().consumerKey
+                        def payload = jwsObject.payload.toJSONObject()
+                        def consumerKey = payload?.consumerKey?:payload?.d?.consumerKey
                         println "consumerKey: " + consumerKey
                         def systemApi = SystemApi.findByApikey(consumerKey)
                         if (!systemApi) {
@@ -38,9 +43,11 @@ class AuthTokenFilters {
                         }
 
                         // 2. Make sure the user is valid
+                        // We have no way to authorize/authenticate the user so we'll just ignore this for now.
 
                         // 3. Make sure there's an issue time and that the token has not expired
-                        //def now = new Date()
+                        // Should implement this validation, but it's not really critical at the moment
+
                     }
                     catch (ParseException e){
                         println("Error parsing JSON web token ${token}: " + e.message)

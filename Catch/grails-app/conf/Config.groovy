59bbbb75e0d9d333702acf5e8a32dcba43cc6ef2
jmiranda <jcm62@columbia.edu>
2014-10-07 19:19:25 -0400

    Removed extra whitespace from Config.groovy

--- Catch/grails-app/conf/Config.groovy
+++ Catch/grails-app/conf/Config.groovy
@@ -13,8 +13,6 @@ grails.config.locations = ["classpath:${appName}-config.properties", "file:./${a
 
 log.info "Using configuration locations ${grails.config.locations}"
 
-
-
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false

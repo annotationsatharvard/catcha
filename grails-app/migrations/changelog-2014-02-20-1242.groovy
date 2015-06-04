2cf679ff2946c34cb325700e17415e00e532aecc
jmiranda <jcm62@columbia.edu>
2015-06-04 15:40:22 -0400

    Existing system APIs should have their secret keys set to api keys until developer has a chance to update (fixed annotationsatharvard/catcha#67)

--- grails-app/migrations/changelog-2014-02-20-1242.groovy
+++ grails-app/migrations/changelog-2014-02-20-1242.groovy
@@ -4,6 +4,17 @@ databaseChangeLog = {
 		addColumn(tableName: "system_api") {
 			column(name: "secret_key", type: "varchar(255)")
 		}
+    }
+
+	changeSet(author: "jmiranda (generated)", id: "1424455190494-2") {
+        grailsChange {
+            change {
+                sql.execute("UPDATE system_api SET secret_key = apikey WHERE secret_key IS NULL")
+                confirm "Successfully set secret key for existing API systems."
+            }
+        }
+
 	}
+
 
 }

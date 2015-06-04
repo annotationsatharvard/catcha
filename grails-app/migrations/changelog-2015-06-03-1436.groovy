b4d598d2a0869f1ff8ca7bbba2bace531030cce1
jmiranda <jcm62@columbia.edu>
2015-06-04 15:39:27 -0400

    Add missing indices on annotation table to improve search API performance (fixed annotationsatharvard/catcha#60)

--- grails-app/migrations/changelog-2015-06-03-1436.groovy
+++ grails-app/migrations/changelog-2015-06-03-1436.groovy
@@ -12,22 +12,36 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "jmiranda (generated)", id: "1433356780261-6") {
-		dropIndex(indexName: "annotation_idx", tableName: "annotation")
-		createIndex(indexName: "annotation_idx", tableName: "annotation") {
-			column(name: "collection_id")
+    changeSet(author: "jmiranda (generated)", id: "1433356780261-3") {
+        createIndex(indexName: "annotation_source_idx", tableName: "annotation") {
+            column(name: "source")
+        }
+    }
 
-			column(name: "context_id")
+    changeSet(author: "jmiranda (generated)", id: "1433356780261-4") {
+        createIndex(indexName: "annotation_uri_idx", tableName: "annotation") {
+            column(name: "uri")
+        }
+    }
 
-			column(name: "media")
 
-			column(name: "source")
-
-			column(name: "uri")
-
+    changeSet(author: "jmiranda (generated)", id: "1433356780261-5") {
+		createIndex(indexName: "annotation_user_idx", tableName: "annotation") {
 			column(name: "userid")
-
 			column(name: "username")
 		}
 	}
+
+    changeSet(author: "jmiranda (generated)", id: "1433356780261-6") {
+        createIndex(indexName: "annotation_media_idx", tableName: "annotation") {
+            column(name: "media")
+        }
+    }
+
+    changeSet(author: "jmiranda (generated)", id: "1433356780261-7") {
+        createIndex(indexName: "annotation_context_idx", tableName: "annotation") {
+            column(name: "collection_id")
+            column(name: "context_id")
+        }
+    }
 }
